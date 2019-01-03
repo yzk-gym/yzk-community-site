0eaaee19371a1838dcbc2a3c46c8ea4b039bcbd0
yuta-ishizaka <zaka@counterworks.jp>
2019-01-03 10:41:39 +0900

    firebaseからデータ取得するように処理変更

--- src/components/EventListView.vue
+++ src/components/EventListView.vue
@@ -15,6 +15,7 @@
 
 <script>
 import EventItem from './EventItem';
+import firestore from '../assets/javascript/firebase';
 
 export default {
   name: 'EventListView',
@@ -22,14 +23,42 @@ export default {
   data() {
     return {
       // テスト用に一時的に中身を作成
-      EventListItems: [
-        { id: 1, title: 'ポケモン駅伝', date: '2018.12.30(日)', time: '13:00', image_path: 'pokemon.png', description: 'ポケモンクリアするまで帰れま10', link_url: 'https://twipla.jp/events/355203' },
-        { id: 2, title: '書き初めしちゃう会', date: '2019.01.03(木)', time: '13:00', image_path: 'kakizome.png', description: '書き初め大会', link_url: 'https://twipla.jp/events/357410' },
-      ], // API経由で取得するため空で初期化する
+      EventListItems: [], // API経由で取得するため空で初期化する
     };
   },
   created() {
-    return null;
+    this.EventListItems = [];
+    firestore.collection('events').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.EventListItems.push(
+          {
+            id: doc.id,
+            title: doc.data().title,
+            date: this.FromTimeStampToDate(doc.data().begin_datetime),
+            time: this.FromTimeStampToTime(doc.data().begin_datetime),
+            image_path: doc.data().image_path,
+            description: doc.data().description,
+            link_url: doc.data().link_url,
+          });
+      });
+    });
+  },
+  methods: {
+    FromTimeStampToDate(date) {
+      const WeekChars = ['日', '月', '火', '水', '木', '金', '土'];
+      const d = new Date(date.seconds * 1000);
+      const year = d.getFullYear();
+      const month = (`0${d.getMonth() + 1}`).slice(-2);
+      const day = (`0${d.getDate()}`).slice(-2);
+      const wday = d.getDay();
+      return `${year}.${month}.${day}(${WeekChars[wday]})`;
+    },
+    FromTimeStampToTime(date) {
+      const d = new Date(date.seconds * 1000);
+      const hour = (`0${d.getHours()}`).slice(-2);
+      const min = (`0${d.getMinutes()}`).slice(-2);
+      return `${hour}:${min}`;
+    },
   },
 };
 </script>
