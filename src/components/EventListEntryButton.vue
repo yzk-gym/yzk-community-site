01b92d5cc867ddb2a8ff00c234ca43e9957c87c7
yuta-ishizaka <zaka@counterworks.jp>
2019-01-03 14:34:18 +0900

    イベント一覧、過去のイベント一覧の実装FIX(ヘッダ、ハンバーガーメニュー以外)

---
+++ src/components/EventListEntryButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="p-button">
+    <p class="c-button-text">{{ text }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'EventListEntryButton',
+  props: {
+    text: { type: String, require: true },
+  },
+};
+</script>
+<style scoped>
+  .p-button {
+    margin: 0 0em;
+    background-color: #FF8A7D;
+    border-radius: 0.2em 0.2em 0.2em 0.2em;
+    text-align: center;
+  }
+  .c-button-text {
+    padding:0.5em;
+    color: white;
+  }
+</style>
