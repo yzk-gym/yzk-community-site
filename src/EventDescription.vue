7c39e157beb24fc949e036be52c0e7c4070275c2
yuta-ishizaka <zaka@counterworks.jp>
2019-01-04 14:44:40 +0900

    ルートコンポーネント作成とルーティング設定まで

---
+++ src/EventDescription.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="event_description">
+    <!--ザンさんがヘッダーつくったらはめる-->
+    <section class="title-section">
+      <content-title2 title="EVENT"></content-title2>
+      <p class="event-title"></p>
+    </section>
+    <top-footer></top-footer>
+  </div>
+</template>
+<script>
+import ContentTitle2 from './components/ContentTitle2';
+import TopFooter from './components/layouts/TopFooter';
+
+export default {
+  name: 'EventDescription',
+  components: {
+    ContentTitle2,
+    TopFooter,
+  },
+};
+</script>
+<style scoped>
+  #event_description {
+    width: 100vw;
+    background-color: #009ACC;
+  }
+  .title-section {
+    padding-top: 1em;
+    padding-bottom: 0.5em;
+    text-align: center;
+  }
+</style>
