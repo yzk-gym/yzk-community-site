3312e7cfe9a231f0fee4064dcd43db69b1a91e81
yuya <siren.night43@gmail.com>
2019-03-01 01:16:17 +0900

    netlify functionの修正

--- functions/ogp.js
+++ functions/ogp.js
@@ -1,7 +1,22 @@
-exports.handler = function(event, context, callback) {
-  console.log('Hello Netlify Functions.')
+import firestore from '../src/assets/javascript/firebase';
+
+exports.handler = function (event, context, callback) {
+  const EVENT_ID = event.queryStringParameters.path;
+  console.log(firestore.collection('events'));
   callback(null, {
     statusCode: 200,
-    body: 'Hello, World',
+    body: `<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>YZKAMP</title>
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:title" content="YZKAMP" />
+    <meta property="twitter:description" content="やってみたいを、やってみよう。" />
+    <meta property="og:url" content="https://yzkamp.netlify.com" />
+    <meta property="og:image" content="https://yzkamp.netlify.com/static/img/yzkamp_ogp.jpg" />
+    <meta http-equiv="refresh" content="0; URL='https://yzkamp.netlify.com/#/events/${EVENT_ID}'" />
+  </head>
+</html>`,
   });
-}
+};
