a4c206f9ac9b17e3bff98d5e2c8895ba375156c2
yuya <siren.night43@gmail.com>
2019-03-06 15:55:28 +0900

    テスト

--- functions/ogp.js
+++ functions/ogp.js
@@ -1,8 +1,8 @@
-const FIRESTORE = require('../src/assets/javascript/firebase');
+const fireStore = require('../src/assets/javascript/firebase');
 
 exports.handler = function (event, context, callback) {
-  const EVENT_ID = event.queryStringParameters.path;
-  console.log(FIRESTORE.collection('events'));
+  const evenntId = event.queryStringParameters.path;
+  console.log(fireStore.collection('events'));
   callback(null, {
     statusCode: 200,
     body: `<html>
@@ -15,7 +15,7 @@ exports.handler = function (event, context, callback) {
     <meta property="twitter:description" content="やってみたいを、やってみよう。" />
     <meta property="og:url" content="https://yzkamp.netlify.com" />
     <meta property="og:image" content="https://yzkamp.netlify.com/static/img/yzkamp_ogp.jpg" />
-    <meta http-equiv="refresh" content="0; URL='https://yzkamp.netlify.com/#/events/${EVENT_ID}'" />
+    <meta http-equiv="refresh" content="0; URL='https://yzkamp.netlify.com/#/events/${evenntId}'" />
   </head>
 </html>`,
   });
